e0ef4d39f92a578cdc6bb38e4e1bdaf991f32485
Angelica Bufano <abufano@tql.com>
2024-06-13 16:32:10 -0400

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Dictionary;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        List<Student> students = new List<Student>();   
+        students.Add(new Student { Id = 1, Name = "Angelica" });
+        students.Add(new Student { Id = 2, Name = "Emma" });
+        students.Add(new Student { Id = 3, Name = "Lauren" });
+
+
+        Dictionary<int, Student> StudentDictionary = new Dictionary<int, Student>();
+        foreach (var student in students) {
+            StudentDictionary.Add(student.Id, student);
+        }
+
+        var Angelica = StudentDictionary[1];
+        Console.WriteLine($"Id: {Angelica.Id}, Name: {Angelica.Name}");
+    }
+}
+
+internal class Student {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
